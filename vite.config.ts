11640f73b6a575f570c1eb2a4031b44bf179bb4b
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-01-28 23:40:16 -0300

    feat: add docker

--- vite.config.ts
+++ vite.config.ts
@@ -38,5 +38,13 @@ export default defineConfig(({ command, mode }) => {
     // configurações de vite
     define: envFiles,
     plugins: [react()],
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true, // needed for the Docker Container port mapping to work
+      strictPort: true,
+      port: 5173, // you can replace this port with any port
+    },
   };
 });
